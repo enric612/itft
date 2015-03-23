d6677e192f88c92cf80ddb3a595250b6e594ab42
Enric Climent <enric@ecliment.ovh>
2015-03-23 11:06:54 +0100

    production mode

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,15 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    #configure production logging of errors
+    if not app.config['DEBUG'] and not app.config['TESTING']:
+        import logging
+        from logging.handlers import SMTPHandler
+        mail_handler = SMTPHandler('127.0.0.1', 'error@example.com',
+                                   app.config['ADMINS'], 'Application Error')
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
     return app
 
 
